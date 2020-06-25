5a14edbf5c9a8b79c889962e4fd13ddf6410ecfa
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-06-25 21:26:45 +0300

    generate-password

--- gen/views.py
+++ gen/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import random
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'gen/index.html')
+
+
+def wordpass(request):
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    p = ''
+
+    if(request.GET.get('uppercase')):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if(request.GET.get('special')):
+        characters.extend(list('!@#$%^&*()'))
+
+    if(request.GET.get('digits')):
+        characters.extend(list('1234567890'))
+
+    length = int(request.GET.get('length', 12))
+
+    for i in range(length):
+        p += random.choice(characters)
+
+    return render(request, 'gen/wordpass.html', {'password': p})
